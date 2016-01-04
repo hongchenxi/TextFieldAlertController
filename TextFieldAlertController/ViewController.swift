915e9cb7259ca743481b2c0a96ba7a6069510a0d
hongchenxi <hongchenxi86@gmail.com>
2016-01-04 18:51:22 +0800

    第一次提交

--- TextFieldAlertController/ViewController.swift
+++ TextFieldAlertController/ViewController.swift
@@ -20,6 +20,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loginAction(sender: AnyObject) {
+        
+        var userNameTextField: UITextField?
+        var pwdTextField: UITextField?
+        
+        let alertController = UIAlertController(title: "登陆", message: "请输入您的用户名和密码", preferredStyle: .Alert)
+        
+        let alertAction = UIAlertAction(title: "登陆", style: .Default) { (action) -> Void in
+            if let username = userNameTextField?.text {
+                print("用户名：\(username)");
+            }else{
+                print("no username");
+            }
+            
+            if let pwd = pwdTextField?.text {
+                print("密码：\(pwd)")
+            }else{
+                print("no pwd")
+            }
+        }
+        
+        alertController.addTextFieldWithConfigurationHandler { (textUserName) -> Void in
+            userNameTextField = textUserName
+            userNameTextField!.placeholder = "您的用户名";
+        }
+        alertController.addTextFieldWithConfigurationHandler { (textPwd) -> Void in
+            pwdTextField = textPwd
+            pwdTextField!.secureTextEntry = true
+            pwdTextField!.placeholder = "您的密码"
+        }
+        
+        alertController.addAction(alertAction)
+        self.presentViewController(alertController, animated: true, completion: nil)
+  
+    }
+    
 
 }
 
